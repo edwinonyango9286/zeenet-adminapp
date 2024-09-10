c467ca6fa1ad2d808df9f14418c8691e63d4e8b4
edwin onyango <ookoedwin29@gmail.com>
2024-09-10 09:51:36 +0300

    updates

--- adminapp/src/index.js
+++ adminapp/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { persistor, store } from "./app/Store";
+import { persistor, store } from "./App/Store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
